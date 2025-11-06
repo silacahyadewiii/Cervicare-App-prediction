6428df434bc27adf86353382c87d0e81b93a5763
Sila <silacahyadewi@students.amikom.ac.id>
2025-11-06 10:21:12 +0700

    fidx to add start.sh for railway deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd backend
-pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+pip install --no-cache-dir -r requirements.txt
+pip install uvicorn
+uvicorn app.main:app --host 0.0.0.0 --port 10000
